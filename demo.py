1d56c63e170e98b88ffe779315d18438d99cec67
Rishini <nawodirishini88@gmail.com>
2024-06-21 14:08:09 +0530

    print statement,data types,escape characters

---
+++ demo.py
@@ -0,0 +1,26 @@
+#print Hello world
+print("Hellow world")
+
+#Data types
+print(123)
+print(1.23)
+print(1+3j)   #complex numbers
+print("Hello Rishini")
+print('Hello Rishini')
+print(True)
+print(False)
+
+#escape characters
+
+#\n new line
+print("Hello")
+print("Rishini")
+
+print("Hello \nRishini")
+
+#\t tab for allign 
+print("Nmae \t: Rishini")
+print("age \t: 23")
+
+#Print Double qutation inside print statement
+print("i said \"You nice\"")
