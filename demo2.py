31f8730e60766380a86e85675d6f33376b0a9d5a
Rishini <nawodirishini88@gmail.com>
2024-06-23 15:04:37 +0530

    Get user input

---
+++ demo2.py
@@ -0,0 +1,27 @@
+a= "10"
+b= 20
+
+c = int(a) + b
+print(c)
+
+#get input from user to execute
+d = input()
+e = 10
+
+f= int(d) + e
+print(f)
+
+#Modify geting input
+print("Enter Your Name: ",end='')
+name = input()
+
+print("Hello" ,name)
+
+#example
+print("Enter Your Name: ",end='')
+name1 = input()
+
+print("Enter Your age: ",end='')
+age = input()
+
+print("Hello" ,name1 ," Your age is" ,age)
