818fd0a232b19793745ce35d67dacaa1cf43ea54
Rishini <nawodirishini88@gmail.com>
2024-06-29 23:25:04 +0530

    add sub two set,in and not in keyword,union,and pipe

--- set1.py
+++ set1.py
@@ -14,3 +14,17 @@ print(x)
 x.remove("World")
 print(x)
 
+a={"rishi","nawo","1"}
+b={"1","2"}
+
+z= a.union(b)  #add two set method 1
+print(z)
+
+z1= a|b#add teow set method 2
+print(z1) 
+
+z2 = a-b
+print(z2)
+
+print("nawo" in z2 )
+print("nawo" not in z2 )
