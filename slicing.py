0cc1a233f6554ddb01fc7b15cf7cc4a2789cf832
Rishini <nawodirishini88@gmail.com>
2024-06-29 23:51:46 +0530

    slicing operator

---
+++ slicing.py
@@ -0,0 +1,16 @@
+#slicing can only do with tuple and list
+
+x=['a','b','c','d','e']
+
+y=x[2]  # secind index value
+y1=x[1:5] #1 to 5 print 5 if numbers not prin untillast 
+
+y2=x[1:5] #until last element
+y3=x[ :5] #start not given but start from 0 possion
+y4=x[1:-1]
+
+print (y)
+print (y1)
+print(y2)
+print(y3)
+print(y4)
