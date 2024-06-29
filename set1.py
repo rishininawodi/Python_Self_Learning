431498effa13fb61d278b5c58dafa5ce905091bd
Rishini <nawodirishini88@gmail.com>
2024-06-29 23:18:01 +0530

    set data stucture operations

---
+++ set1.py
@@ -0,0 +1,16 @@
+x={"Hello","World"}
+
+print (x)
+
+x1 = {"Hello","World","Hello"}
+print(x1) #same out put.not get dublicate value and no sequenciaal manner
+
+x.add("World")
+print(x)
+
+x.add("world")#add to set because simple W 
+print(x)
+
+x.remove("World")
+print(x)
+
