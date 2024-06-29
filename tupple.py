d6916a89d1e87eab5671c0c7707b212719c36eda
Rishini <nawodirishini88@gmail.com>
2024-06-29 23:37:52 +0530

    Tuple data structure

---
+++ tupple.py
@@ -0,0 +1,14 @@
+myself= ("Rishini" , 22,"Thanamlwila","student")
+print(type(myself))
+print(myself)
+
+name=myself[0]
+print(name)
+
+print(myself.count(22))
+
+index,name1,height,village = myself  #expand tuple data in to variables.need same number of variable
+print(index,name,height)
+
+sss = myself
+print(sss)
