a122c25428ac0b0997a5a003502124d23bcb8ac0
Rishini <nawodirishini88@gmail.com>
2024-06-30 16:52:14 +0530

    Condition part 3

---
+++ condition2.py
@@ -0,0 +1,23 @@
+#method 1
+height = 176
+
+if height > 150:
+    print("security")
+else:
+    print("labor")    
+
+#method 2
+height2 = 176
+
+if height2 > 150:
+    job ="security"
+else:
+    job ="labor"
+
+print(job) 
+
+#method 3
+height3=140
+msg = "security" if height3 >150 else "labour"
+
+print(msg)
