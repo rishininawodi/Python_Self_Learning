1050ea789cd8ae18f0f87994d840fb0698d5ef15
Rishini <nawodirishini88@gmail.com>
2024-06-30 16:24:56 +0530

    Condition simple if else part

---
+++ condition.py
@@ -0,0 +1,19 @@
+x= 10
+
+if x >= 150:
+    print("Seleect")
+
+else:
+    print("Not selected")
+
+# 2nd method
+marks = 50
+result = "" #or " None" use  #if result not define then if first if fail come err.if first one pass no err.so nee define rresult variable...
+if marks > 50:
+    result = "pass"
+
+else:
+    result ="fail"    
+
+print(result)    
+      
