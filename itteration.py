d5c810163abc52143fbf67751a8bb9a20df6131a
Rishini <nawodirishini88@gmail.com>
2024-07-02 16:07:14 +0530

    itteration

---
+++ itteration.py
@@ -0,0 +1,15 @@
+d={
+    "Praneeth":176,
+    "rishini": 233,
+    "hjjnl":567,
+    "gghgb":88
+}
+
+for name in d:
+    height =d[name]
+    print(name)
+
+
+for name1 in d:
+    height =d[name]
+    print(name1,height)    
